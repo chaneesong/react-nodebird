46de897f4464123b0ff32e987226f24e7fcd4276
gigo96 <sch102403@gmail.com>
2022-11-07 14:54:55 +0900

    test: 백엔드 구현 전 렌더링 확인을 위해 더미 데이터 구성

--- prepare/components/AppLayout.js
+++ prepare/components/AppLayout.js
@@ -49,7 +49,11 @@ const AppLayout = ({ children }) => {
       <Menu mode='horizontal' items={menuItems} />
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          {isLoggedIn ? <UserProfile /> : <LoginForm />}
+          {isLoggedIn ? (
+            <UserProfile />
+          ) : (
+            <LoginForm setIsLoggedIn={setIsLoggedIn} />
+          )}
         </Col>
         <Col xs={24} md={12}>
           {children}
