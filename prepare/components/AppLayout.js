13afcc4943d21526d0be40bfb81dbfe73b1e8dc7
gigo96 <sch102403@gmail.com>
2022-11-07 14:40:16 +0900

    feat: 로그인 여부에 따른 화면 렌더링
    로그인 여부에 따라 회원가입 화면과 프로핑 화면을 렌더링 하도록 변경
    
    refactor: 검색 style 인라인에서 컴포넌트로 변경
    
    인라인으로 할 경우 이전과 변화가 없음에도 리렌더링 되는 성능 저하 발생

--- prepare/components/AppLayout.js
+++ prepare/components/AppLayout.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { Menu, Input, Row, Col } from 'antd';
+import styled from 'styled-components';
+
+import LoginForm from '../components/LoginForm';
+import UserProfile from '../components/UserProfile';
+
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`;
 
 const menuItems = [
   {
@@ -22,7 +30,7 @@ const menuItems = [
   },
   {
     key: 'Search',
-    label: <Input.Search style={{ verticalAlign: 'middle' }} enterButton />,
+    label: <SearchInput enterButton />,
   },
   {
     key: 'SignUp',
@@ -35,18 +43,19 @@ const menuItems = [
 ];
 
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <>
       <Menu mode='horizontal' items={menuItems} />
-      <Row>
+      <Row gutter={8}>
         <Col xs={24} md={6}>
-          left
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
         </Col>
         <Col xs={24} md={12}>
           {children}
         </Col>
         <Col xs={24} md={6}>
-          right
+          <div>section 3</div>
         </Col>
       </Row>
     </>
