c0e301ef9974deb6d841e601a05d11b735f95f08
gigo96 <sch102403@gmail.com>
2022-11-08 17:39:11 +0900

    feat: 유저 프로필 페이지 화면 구현
    profile.js
    1. 백엔드 구현 전 더미데이터 입력
    2. nickname 변경 컴포넌트 연결
    3. follow list 출력 화면 연결
    
    NicknameEditForm.js
    1. antd search 컴포넌트를 활용하여 nickname 변경 화면 구현
    
    FollowList.js
    1. antd list 컴포넌트 활용하여 follow 목록을 보여주는 화면 구현

--- prepare/pages/profile.js
+++ prepare/pages/profile.js
@@ -1,11 +1,33 @@
+import Head from 'next/head';
 import React from 'react';
+
 import AppLayout from '../components/AppLayout';
+import NicknameEditForm from '../components/NicknameEditForm';
+import FollowList from '../components/FollowList';
 
 const Profile = () => {
+  const followerList = [
+    { nickname: 'song' },
+    { nickname: 'chang' },
+    { nickname: 'hee' },
+  ];
+  const followingList = [
+    { nickname: 'song' },
+    { nickname: 'chang' },
+    { nickname: 'hee' },
+  ];
+
   return (
-    <AppLayout>
-      <div>프로필 페이지</div>
-    </AppLayout>
+    <>
+      <Head>
+        <title>Nodebird | Profile</title>
+      </Head>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header='팔로잉 목록' data={followingList} />
+        <FollowList header='팔로워 목록' data={followerList} />
+      </AppLayout>
+    </>
   );
 };
 
