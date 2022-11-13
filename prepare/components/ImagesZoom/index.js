c921526284b36847f0836a386d555004492bed28
gigo96 <sch102403@gmail.com>
2022-11-14 00:04:27 +0900

    feat: 이미지 폼 구현
    1. 이미지 1장, 2장, 3장 이상일 때 다른 폼 구현
    2. 이미지 캐루셀 구현 react-slick 사용
    3. 주요 컴포넌트와 style 컴포넌트 분리

---
+++ prepare/components/ImagesZoom/index.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Slick from 'react-slick';
+
+import {
+  Global,
+  Overlay,
+  Header,
+  CloseButton,
+  SlickWrapper,
+  ImageWrapper,
+  Indicator,
+} from './styles';
+
+const ImagesZoom = ({ images, onClose }) => {
+  const [CurrentSlide, setCurrentSlide] = useState(0);
+
+  return (
+    <Overlay>
+      <Global />
+      <Header>
+        <h1>상세 이미지</h1>
+        <CloseButton onClick={onClose}>X</CloseButton>
+      </Header>
+      <SlickWrapper>
+        <div>
+          <Slick
+            initialSlide={0}
+            beforeChange={(slide, newSlide) => setCurrentSlide(newSlide)}
+            infinite
+            arrows={false}
+            slidesToShow={1}
+            slidesToScroll={1}
+          >
+            {images.map((image) => (
+              <ImageWrapper key={image.src}>
+                <img src={image.src} alt={image.src} />
+              </ImageWrapper>
+            ))}
+          </Slick>
+          <Indicator>
+            <div>
+              {CurrentSlide + 1} / {images.length}
+            </div>
+          </Indicator>
+        </div>
+      </SlickWrapper>
+    </Overlay>
+  );
+};
+
+ImagesZoom.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default ImagesZoom;
