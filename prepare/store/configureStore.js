f555194971c8aedafb0bb2c660d330904849c9bb
gigo96 <sch102403@gmail.com>
2022-11-16 00:56:37 +0900

    쓸모없는 코드 제거

--- prepare/store/configureStore.js
+++ prepare/store/configureStore.js
@@ -1,21 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { createWrapper } from 'next-redux-wrapper';
 import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from '../reducers';
+import rootSaga from '../sagas';
 
 const isDev = process.env.NODE_ENV === 'development';
 
 const createStore = () => {
-  const middleware = [];
-  if (isDev) {
-    middleware.concat(logger);
-  }
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware, logger];
+
   const store = configureStore({
     reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(middlewares),
     devTools: isDev,
   });
+
+  store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 
