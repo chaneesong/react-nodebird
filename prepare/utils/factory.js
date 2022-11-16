0f399a6b85a66d57740b7cb2786b0e0613e9cac0
gigo96 <sch102403@gmail.com>
2022-11-16 23:04:00 +0900

    feat: 비동기 action을 만드는 함수 구현

---
+++ prepare/utils/factory.js
@@ -0,0 +1,5 @@
+export const createAsyncActions = (suffixOfActions) => ({
+  request: suffixOfActions + '_REQUEST',
+  success: suffixOfActions + '_SUCCESS',
+  failure: suffixOfActions + '_FAILURE',
+});
