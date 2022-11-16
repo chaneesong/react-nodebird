3fffa35302b58cefa62fe8f7c71a0358d5d80519
gigo96 <sch102403@gmail.com>
2022-11-16 23:16:58 +0900

    feat: action들을 따로 담아두는 actions 디렉토리 추가
    리듀서의 디렉토리 구조를 따른다.

---
+++ prepare/actions/post.js
@@ -0,0 +1,4 @@
+import { createAsyncActions } from '../utils/factory';
+
+export const ADD_POST = createAsyncActions('ADD_POST');
+export const ADD_COMMENT = createAsyncActions('ADD_COMMENT');
---
+++ prepare/actions/user.js
@@ -0,0 +1,7 @@
+import { createAsyncActions } from '../utils/factory';
+
+export const LOG_IN = createAsyncActions('LOG_IN');
+export const LOG_OUT = createAsyncActions('LOG_OUT');
+export const SIGN_UP = createAsyncActions('SIGN_UP');
+export const FOLLOW = createAsyncActions('FOLLOW');
+export const UNFOLLOW = createAsyncActions('UNFOLLOW');
