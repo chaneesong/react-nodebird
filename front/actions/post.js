1780cfdd8fb32cd009a2441bc64e5c1d4eae8d6d
gigo96 <sch102403@gmail.com>
2022-11-19 20:46:35 +0900

    feat: 게시글 삭제 action 구현

--- front/actions/post.js
+++ front/actions/post.js
@@ -2,3 +2,4 @@ import { createAsyncActions } from '../utils/factory';
 
 export const ADD_POST = createAsyncActions('ADD_POST');
 export const ADD_COMMENT = createAsyncActions('ADD_COMMENT');
+export const REMOVE_POST = createAsyncActions('REMOVE_POST');
