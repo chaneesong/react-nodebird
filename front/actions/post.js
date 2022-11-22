f968769fcf1c220eae3b77cf2ebb593bd05cf3d7
gigo96 <sch102403@gmail.com>
2022-11-23 02:35:21 +0900

    feat: 포스트 로딩 비동기 액션 정의

--- front/actions/post.js
+++ front/actions/post.js
@@ -3,3 +3,4 @@ import { createAsyncActions } from '../utils/factory';
 export const ADD_POST = createAsyncActions('ADD_POST');
 export const ADD_COMMENT = createAsyncActions('ADD_COMMENT');
 export const REMOVE_POST = createAsyncActions('REMOVE_POST');
+export const LOAD_POSTS = createAsyncActions('LOAD_POST');
