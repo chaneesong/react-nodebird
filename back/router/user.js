1ba086735a2c1a912b00cbddb46532366f97c645
gigo96 <sch102403@gmail.com>
2022-11-30 10:53:41 +0900

    user router 구현중

--- back/router/user.js
+++ back/router/user.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import { User } from '../models/index.js';
 
 const router = express.Router();
+
+router.post('/', async (req, res) => {
+  await User.create({
+    email: req.body.email,
+    nickname: req.body.nickname,
+    password: req.body.password,
+  });
+  req.json();
+});
 
 export default router;
