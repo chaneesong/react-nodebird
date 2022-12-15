0aa8dd680ecdc05740de37908645b26a4e9644b9
송창희 <sch102403@gmail.com>
2022-12-16 04:44:00 +0900

    feat: 공유를 위해 개인 게시글 분리 라우터 구현
    하나의 게시글을 화면에 보여주기 위해 id를 파라미터로 사용하는 동적 라우터 구현
    id를 받아 해당 게시글을 따로 보여줌
    이 때 ssr을 위해 getServerSideProps 사용
    저장소에 siglePost 추가
    state 및 비동기 요청을 위해 reducer와 saga [ get /post/postId ] 추가

--- front/actions/post.js
+++ front/actions/post.js
@@ -4,6 +4,7 @@ export const ADD_POST = createAsyncActions('ADD_POST');
 export const ADD_COMMENT = createAsyncActions('ADD_COMMENT');
 export const REMOVE_POST = createAsyncActions('REMOVE_POST');
 export const LOAD_POSTS = createAsyncActions('LOAD_POSTS');
+export const LOAD_POST = createAsyncActions('LOAD_POST');
 export const LIKE_POST = createAsyncActions('LIKE_POST');
 export const UNLIKE_POST = createAsyncActions('UNLIKE_POST');
 export const UPLOAD_IMAGES = createAsyncActions('UPLOAD_IMAGE');
---
+++ front/pages/post/[id].js
@@ -0,0 +1,56 @@
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import { END } from 'redux-saga';
+import { useSelector } from 'react-redux';
+import Head from 'next/head';
+
+import wrapper from '../../store/configureStore';
+import { LOAD_POST } from '../../actions/post';
+import { LOAD_MY_INFO } from '../../actions/user';
+import AppLayout from '../../components/AppLayout';
+import PostCard from '../../components/PostCard';
+
+const Post = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const { singlePost } = useSelector((state) => state.post);
+
+  return (
+    <AppLayout>
+      <Head>
+        <title>{singlePost.User.nickname}의 게시글</title>
+        <meta
+          property='og:title'
+          content={`${singlePost.User.nicname}님의 게시글`}
+        />
+        <meta property='og:description' content={singlePost.content} />
+        <meta property='og:image' content={singlePost.content} />
+        <meta property='og:url' content={`http://localhost:3060/post/${id}`} />
+      </Head>
+      singlePost ? <PostCard post={singlePost} />
+    </AppLayout>
+  );
+};
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) =>
+    async ({ req, params }) => {
+      const cookie = req?.headers.cookie;
+      axios.defaults.headers.Cookie = '';
+
+      if (req && cookie) {
+        axios.defaults.headers.Cookie = cookie;
+      }
+      store.dispatch({
+        type: LOAD_MY_INFO.request,
+      });
+      store.dispatch({
+        type: LOAD_POST.request,
+        data: params.id,
+      });
+      store.dispatch(END);
+      await store.sagaTask.toPromise();
+    }
+);
+
+export default Post;
